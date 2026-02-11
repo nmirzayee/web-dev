af89621641565837810333e64e06bf5f78cdb0f2
Nematullah Mirzayee <n_mirzayee@kbtu.kz>
2026-02-12 01:48:00 +0500

    Add files via upload

---
+++ Lab3/Task1/javascriptFundementals/codestructure.js
@@ -0,0 +1,16 @@
+
+alert('Hello');
+alert('World');
+
+alert(3 +
+1
++ 2);
+
+// This comment occupies a line of its own
+alert('Hello');
+
+alert('World'); // This comment follows the statement
+
+/* An example with two messages.
+This is a multiline comment.
+*/
---
+++ Lab3/Task1/javascriptFundementals/variable.js
@@ -0,0 +1,13 @@
+let admin, name;  
+name = "John";
+
+admin = name;
+
+alert( admin ); // "John"
+
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+
+const birthday = '18.04.1982';
